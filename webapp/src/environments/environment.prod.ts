3b5480843e8532c6f59500d88ef6a39808e62a6b
sheezan-yo <khansheezan30@gmail.com>
2025-07-25 22:35:45 +0530

    fix

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: "https://meanprojecom.onrender.com/api"
+    apiUrl: "https://meanprojecom.onrender.com"
     // apiUrl: 'http://myshop.test'
 };
